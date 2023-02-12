19ca4a2c2307f53653718aa4629bf73db04a5c98
kenpavalko <kpavalko@the-itdepartment.com>
2023-02-12 00:51:08 -0500

    added static root

--- project/settings.py
+++ project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'skeet_shooting_scoring_app_db',
         'USER' : 'postgres',
-        'PASSWORD' : 'Post6163',
+        'PASSWORD' : 'Post!!036163',
         'HOST' : 'localhost',
     }
 }
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
