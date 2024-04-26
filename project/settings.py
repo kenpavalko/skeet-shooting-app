5074a0daa1e37bb73a0107f9eca8927a0fffbe89
kenpavalko <kpavalko@the-itdepartment.com>
2024-04-26 19:21:53 +0000

    updated requirements and settings file for static files

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'indigoshoot/static'
