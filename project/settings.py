1b5bf2474be90bff25dd3dc116e06396d33e5c23
martypavalko <mpavalko@avifoodsystems.com>
2025-02-13 20:19:36 -0500

    Added ability to reference environment variables

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#&-arh2q48()xa278zs2y!k&h8!n)k6oz%^goc)dx__du^53r'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['indigoshootscores.myitdepartment.org', 'localhost', 'skeet-shooting-app-production.up.railway.app']
+# ALLOWED_HOSTS = ['indigoshootscores.myitdepartment.org', 'localhost', 'skeet-shooting-app-production.up.railway.app']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
 
@@ -130,13 +134,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'indigoshoot/static'
-]
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = 'static/'
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'indigoshoot/static'
+# ]
 
 # Media Folder settings
 MEDIA_ROOT = BASE_DIR / 'media'
